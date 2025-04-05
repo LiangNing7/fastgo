c199c65ebea022942d70ba779cf8ab295db2439b
凉柠 <liangning2277@gmail.com>
2025-04-05 08:08:33 +0800

    feat: 基于Gin实现HTTP服务器

--- cmd/fg-apiserver/app/options/options.go
+++ cmd/fg-apiserver/app/options/options.go
@@ -2,18 +2,24 @@
 package options
 
 import (
+	"fmt"
+	"net"
+	"strconv"
+
 	"github.com/LiangNing7/fastgo/internal/apiserver"
 	genericoptions "github.com/LiangNing7/fastgo/pkg/options"
 )
 
 type ServerOptions struct {
 	MySQLOptions *genericoptions.MySQLOptions `json:"mysql" mapstructure:"mysql"`
+	Addr         string                       `json:"addr" mapstructure:"addr"`
 }
 
 // NewServerOptions 创建带有默认值的 ServerOptions 实例.
 func NewServerOptions() *ServerOptions {
 	return &ServerOptions{
 		MySQLOptions: genericoptions.NewMySQLOptions(),
+		Addr:         "0.0.0.0:6666",
 	}
 }
 
@@ -24,6 +30,23 @@ func (o *ServerOptions) Validate() error {
 		return err
 	}
 
+	// 验证服务器地址
+	if o.Addr == "" {
+		return fmt.Errorf("server address cannot be empth")
+	}
+
+	// 检查地址格式是否为 host:post
+	_, portStr, err := net.SplitHostPort(o.Addr)
+	if err != nil {
+		return fmt.Errorf("invalid server address format '%s': %w", o.Addr, err)
+	}
+
+	// 验证端口是否为数字且在范围内
+	port, err := strconv.Atoi(portStr)
+	if err != nil || port < 1 || port > 66635 {
+		return fmt.Errorf("invalid server port: %s", portStr)
+	}
+
 	return nil
 }
 
@@ -31,5 +54,6 @@ func (o *ServerOptions) Validate() error {
 func (o *ServerOptions) Config() (*apiserver.Config, error) {
 	return &apiserver.Config{
 		MySQLOptions: o.MySQLOptions,
+		Addr:         o.Addr,
 	}, nil
 }
